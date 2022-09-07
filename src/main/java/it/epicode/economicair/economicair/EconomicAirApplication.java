9d3b5977eccc14512448e31b372cfe1560581d5e
SaraMottolese <saramottolese89@yahoo.it>
2022-09-07 16:15:35 +0200

    init project

---
+++ src/main/java/it/epicode/economicair/economicair/EconomicAirApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.economicair.economicair;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomicAirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomicAirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/epicode/economicair/economicair/EconomicAirApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.economicair.economicair;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomicAirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
