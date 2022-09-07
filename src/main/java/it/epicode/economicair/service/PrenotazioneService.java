1cf1101a3969c2ae9612550947c602f20d9bee7d
mastinovts <mastinovts@gmail.com>
2022-09-07 17:58:51 +0200

    Work in progress: Service Layer

--- src/main/java/it/epicode/economicair/service/PrenotazioneService.java
+++ src/main/java/it/epicode/economicair/service/PrenotazioneService.java
@@ -1,8 +1,33 @@
 package it.epicode.economicair.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import it.epicode.economicair.model.Prenotazione;
+import it.epicode.economicair.repository.PrenotazioneRepository;
 
 @Service
 public class PrenotazioneService {
+	
+	@Autowired
+	PrenotazioneRepository prenotazioneRepository;
+	
+	public Prenotazione aggiungi(Prenotazione p) {
+		return prenotazioneRepository.save(p);
+	}
+	
+	public void elimina(Long id) {
+		prenotazioneRepository.deleteById(id);
+	}
+	
+	public List<Prenotazione> getAll(){
+		return prenotazioneRepository.findAll();
+	}
+	
+	public Prenotazione aggiornaPrenotazione(Prenotazione p, Long id) {
+		return prenotazioneRepository.save(p);
+	}
 
 }
