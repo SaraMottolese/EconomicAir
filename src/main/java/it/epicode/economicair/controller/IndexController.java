b632063825d34af1a943cc030c56c26361d8f12d
chrisjusel <christian00.vitale@gmail.com>
2022-09-08 17:25:09 +0200

    thymeleaf e index addedd

---
+++ src/main/java/it/epicode/economicair/controller/IndexController.java
@@ -0,0 +1,20 @@
+/**
+ * Questa classe ha il solo scopo di reindirizzare l'utente alla pagina
+ * index quando va sull'url dell'applicazione
+ */
+package it.epicode.economicair.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+	
+	@GetMapping("/index")
+	public ModelAndView index() {
+		return new ModelAndView("index");
+	}
+	
+}
