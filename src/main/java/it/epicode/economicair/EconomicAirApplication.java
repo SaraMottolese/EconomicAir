767051087de9097a3c4d299f815cb9af4c8109ac
mastinovts <mastinovts@gmail.com>
2022-09-09 15:33:58 +0200

    Clean Code

--- src/main/java/it/epicode/economicair/EconomicAirApplication.java
+++ src/main/java/it/epicode/economicair/EconomicAirApplication.java
@@ -1,16 +1,8 @@
 package it.epicode.economicair;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import it.epicode.economicair.model.Aeroporto;
-import it.epicode.economicair.util.MyCsvReader;
 
 @SpringBootApplication
 public class EconomicAirApplication {
